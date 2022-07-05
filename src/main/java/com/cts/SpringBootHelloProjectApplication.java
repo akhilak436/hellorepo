76ed7f8531a1d3d77fb7bc49801bee386c6fcea1
akhila <akhilak436@gmail.com>
2022-07-05 11:33:19 +0530

    first commit

---
+++ src/main/java/com/cts/SpringBootHelloProjectApplication.java
@@ -0,0 +1,20 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootHelloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloProjectApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String getMsg() {
+		return "hello welcome to aws ecs";
+	}
+}
